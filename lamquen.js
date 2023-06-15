2250aef37ece43ee9bb49d5fe26d85b24c2b502d
phammanhthang1911 <gauxam5@gmail.com>
2023-06-15 21:42:56 +0700

    5555555

---
+++ lamquen.js
@@ -0,0 +1,22 @@
+var course =[
+    {
+        id:1,
+        name:"thang",
+        coin:500
+    }
+    {
+        id:2,
+        name:"pham",
+        coin:250 
+    }
+    {
+        id:3,
+        name:"manh",
+        
+        coin:0
+    }
+];
+    course.forEach(function(course,index){
+        console.log(index,course);});
+    
+
